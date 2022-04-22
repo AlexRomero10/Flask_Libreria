fe3a9ed922af1b3d0a6cab0bac1f003b7b7b5770
Alejandro Romero <aletromp00@gmail.com>
2022-04-22 15:32:18 +0200

    He modificado el fichero app.py

--- app.py
+++ app.py
@@ -1,24 +1,40 @@
-from distutils.log import debug
+# En el siguiente programa realizado en python vamos a trabajar sobre un fichero .json y con flask para generar paginas dinamicas.
+
 from flask import Flask, render_template, abort
-import os
 import json
-app = Flask (__name__)
-with open("books.json") as fichero:
-    datos=json.load(fichero)
-@app.route('/',methods=["GET","POST"])
+import os
+
+
+app=Flask(__name__)
+
+#Creamos la variables global libros, que usaremos en nuestras paginas dinamicas para mostrar la informacion de los libros.
+
+with open("books.json") as libros:
+    datos=json.load(libros)
+
+#Creamos nuestra primera ruta que nos dirigira a la pagina principal en la que podremos ver todos los enlaces de los libros para ver su informacion.
+
+@app.route('/')
 def inicio():
-    return render_template("inicio.html",libros=datos)
-@app.route('/libro/<isbn>',methods=["GET","POST"])
+    nombre='Alejandro Romero Tortosa'
+    return render_template('inicio.html', libros=datos, nombre=nombre)
+
+#Creamos nuestra segunda ruta en nuestro programa que nos ayudara a obtener la informacion de los libros
+
+@app.route('/libro/<isbn>')
 def libro(isbn):
-    for book in datos:
-        if "isbn" in book.keys() and isbn == book["isbn"]:
-            return render_template("libro.html", libro=book)
-    abort(404)
-@app.route('/categoria/<categoria>',)
+    for l in datos:
+
+        #Aqui debemos hacer una comprobacion doble ya que hay algunos libros que no tienen isbn, por lo que eso se debe comprobar y ademas
+            
+        if "isbn" in l.keys() and isbn == l["isbn"]:
+            return render_template('libros.html', libro=l) 
+
+    return abort(404)
+
+@app.route('/categoria/<categoria>')
 def categoria(categoria):
-    for category in datos:
-        if "categories" in category.keys() and categoria in category["categories"]:
-            return render_template("categoria.html", libros=datos, categoria=categoria)
-    abort(404)
+            return render_template('categoria.html', libros=datos, categoria=categoria)
+
 
 app.run('0.0.0.0',debug=False)
